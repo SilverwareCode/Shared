d3cdca22ed0ad93b2bcd9ca3079299128599f9da
SilverwareCode <matous.havranek@gmail.com>
2026-04-23 22:36:11 +0200

    p

--- OCI/js/remoteContentLoader.js
+++ OCI/js/remoteContentLoader.js
@@ -1,8 +1,6 @@
 async function getRemoteContent(targetContainerId, url) {
     const kontejner = document.getElementById(targetContainerId);
     // DŮLEŽITÉ: Musí to být adresa začínající raw.githubusercontent.com
-    //const url = 'https://raw.githubusercontent.com/SilverwareCode/Shared/refs/heads/main/OCI/Marketing/MarketingPackages/TestPackage/index.html';
-
 
     if (kontejner == null) {
         alert("kontejner " + targetContainerId +" null");
@@ -14,6 +12,7 @@ async function getRemoteContent(targetContainerId, url) {
 
         const text = await response.text();
         kontejner.innerHTML = text;
+
     } catch (error) {
         console.error('Chyba:', error);
         kontejner.innerHTML = 'Chyba při načítání obsahu z GitHubu.';
