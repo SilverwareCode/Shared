b777fdb81827a47863cba790e6481d93850bf0bc
SilverwareCode <matous.havranek@gmail.com>
2026-04-23 21:22:04 +0200

    aaa

--- OCI/js/remoteContentLoader.js
+++ OCI/js/remoteContentLoader.js
@@ -3,6 +3,11 @@ async function getRemoteContent(targetContainerId, url) {
     // DŮLEŽITÉ: Musí to být adresa začínající raw.githubusercontent.com
     //const url = 'https://raw.githubusercontent.com/SilverwareCode/Shared/refs/heads/main/OCI/Marketing/MarketingPackages/TestPackage/index.html';
 
+
+    if (kontejner == null) {
+        alert("kontejner " + targetContainerId +" null");
+    }
+
     try {
         const response = await fetch(url);
         if (!response.ok) throw new Error('Nepodařilo se stáhnout soubor');
